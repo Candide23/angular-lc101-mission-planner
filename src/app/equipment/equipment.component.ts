12324c02ad31fb4fd2b294249b7909f661d8948e
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-03-31 11:00:30 -0500

    completed first part of studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
-  constructor() { }
+equipmentItems: object[] = [
+       {name: 'Duct Tape', mass: 0.5},
+       {name: 'Space Camera', mass: 20},
+       {name: 'Food', mass: 150},
+       {name: 'Oxygen Tanks', mass: 400},
+       {name: 'AE-35 Unit', mass: 5},
+       {name: 'ISS Supplies', mass: 800},
+       {name: 'Water', mass: 250},
+       {name: 'Satellite', mass: 1200},
+       {name: 'R2 Unit', mass: 32}
+   ];
+   cargoHold: object[] = [];
+   cargoMass: number = 0;
+   maximumAllowedMass: number = 2000;
+   maxItems: number = 10;
+ 
 
-  ngOnInit() {
+   constructor() { }
+
+   ngOnInit() { }
+
+   // Code your addItem function here:
+   addItem(equipmentItem : object, boolean) {
+    //  if (!this.cargoHold.includes(equipmentObject){
+      this.cargoHold.push(equipmentItem);
+      this.cargoMass += equipmentItem['mass'];
+    //  }
+
+    return this.cargoMass +200 >= this.maximumAllowedMass 
+    
+   }
+
+   
+
+   isDisabled(item) {
+    return this.cargoHold.length === this.maxItems || item.mass + this.cargoMass > this.maximumAllowedMass
   }
+
+
+  clearHold(){
+    this.cargoHold = [];
+    this.cargoMass = 0;
+  }
+
+  
+
 
 }
